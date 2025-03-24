24b52b3cf20bf7247305b37391fe2d2787ef0a5b
Alex Uribe <repzyyog@gmail.com>
2025-03-24 19:18:36 +0100

    Create adviser type

--- src/characters/types.ts
+++ src/characters/types.ts
@@ -11,3 +11,8 @@ type King = Character & {
   yearsOfReign: number;
   phrase: "You are all going to die";
 };
+
+type Adviser = Character & {
+  advisesTo: string;
+  phrase: "I don't know why, but I think I'm going to die soon";
+};
