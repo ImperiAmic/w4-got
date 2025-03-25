daa72513f669ceb5e899186830a9744fcf544578
Raimon <raimon.txollo@gmail.com>
2025-03-25 23:59:48 +0100

    Fix use of individual types by creating a new Characters type of types

--- src/character/getPhrases.ts
+++ src/character/getPhrases.ts
@@ -1,11 +1,6 @@
-import { Adviser } from "../adviser/types";
-import { Fighter } from "../fighter/types";
-import { King } from "../king/types";
-import { Squire } from "../squire/types";
+import { Characters } from "./types";
 
-const getPhrases = (
-  characters: (King | Adviser | Fighter | Squire)[],
-): string[] => {
+const getPhrases = (characters: Characters[]): string[] => {
   const phrases: string[] = [];
 
   characters.map((character) => phrases.push(character.phrase));
--- src/character/types.ts
+++ src/character/types.ts
@@ -1,3 +1,8 @@
+import { Adviser } from "../adviser/types";
+import { Fighter } from "../fighter/types";
+import { King } from "../king/types";
+import { Squire } from "../squire/types";
+
 export type Character = {
   name: string;
   lastName: string;
@@ -6,3 +11,5 @@ export type Character = {
   portraitUrl: string;
   portraitDescription: string;
 };
+
+export type Characters = Adviser | Fighter | King | Squire;
