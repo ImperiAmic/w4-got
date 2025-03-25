ef3bc4d74bee4508db809361f5dd0ac2ffd88c88
Alex Uribe <repzyyog@gmail.com>
2025-03-25 21:02:46 +0100

    Fix package-lock.json merge

--- jest.config.ts
+++ jest.config.ts
@@ -3,11 +3,8 @@ import { createDefaultPreset, JestConfigWithTsJest } from "ts-jest";
 const config: JestConfigWithTsJest = {
   verbose: true,
   rootDir: "src",
-<<<<<<< HEAD
   testEnvironment: "jsdom",
   resolver: "ts-jest-resolver",
-=======
->>>>>>> main
   ...createDefaultPreset(),
 };
 
