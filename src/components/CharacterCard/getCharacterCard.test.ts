aa995cc1a2d92fb16a46870348976506b685f43e
Raimon <raimon.txollo@gmail.com>
2025-03-26 21:04:17 +0100

    Add CharacterCard component with its function and test

---
+++ src/components/CharacterCard/getCharacterCard.test.ts
@@ -0,0 +1,33 @@
+import { CharacterType } from "../../character/types";
+import getCharacterCard from "./getCharacterCard";
+
+describe("Given the CharacterCard component", () => {
+  describe("When it renders the Felipe character", () => {
+    test("Then it should show the Felipe character name and last name inside a card", () => {
+      const screen = document.createElement("div");
+      const felipe: CharacterType = {
+        name: "Felipe",
+        lastName: "Sesto",
+        age: 0,
+        isAlive: true,
+        die: () => {},
+        yearsOfReign: 0,
+        phrase: "You are all going to die",
+        portraitUrl: "",
+        portraitDescription: "",
+        speak: "",
+      };
+      const expectedCardTitle = "Felipe Sesto";
+
+      const CharacterCard = getCharacterCard(felipe);
+
+      screen.appendChild(CharacterCard);
+
+      const CardArticle = screen.querySelector("li");
+      const CardTitle = screen.querySelector("h2");
+      expect(CardArticle).not.toBeNull();
+      expect(CharacterCard).not.toBeNull();
+      expect(CardTitle?.textContent).toBe(expectedCardTitle);
+    });
+  });
+});
---
+++ src/components/CharacterCard/getCharacterCard.ts
@@ -0,0 +1,15 @@
+import { CharacterType } from "../../character/types";
+
+const getCharacterCard = (character: CharacterType): HTMLElement => {
+  const characterCardElement = document.createElement("li");
+  characterCardElement.className = "character";
+
+  characterCardElement.innerHTML = `
+  <article>
+    <h2 class="character-title">${character.name} ${character.lastName}</h2>
+  </article>`;
+
+  return characterCardElement;
+};
+
+export default getCharacterCard;
