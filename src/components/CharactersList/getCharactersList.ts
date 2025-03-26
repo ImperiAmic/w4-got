01b6c9b60681e4acc3861eb04978bc030f84fb85
Raimon <raimon.txollo@gmail.com>
2025-03-26 21:05:24 +0100

    Add render of CharacterCard to CharactersList

--- src/components/CharactersList/getCharactersList.ts
+++ src/components/CharactersList/getCharactersList.ts
@@ -1,6 +1,12 @@
+import { characters } from "../../character/data/data.js";
+import getCharacterCard from "../CharacterCard/getCharacterCard.js";
+
 const getCharactersList = (): HTMLElement => {
   const charactersElement = document.createElement("ul");
   charactersElement.className = "characters";
+
+  const CharacterCard = getCharacterCard(characters[0]);
+  charactersElement.appendChild(CharacterCard);
 
   return charactersElement;
 };
