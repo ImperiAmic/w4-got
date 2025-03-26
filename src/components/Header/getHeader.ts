fdb0030038d517ae540e3aed0243f04fd6a02448
Raimon <raimon.txollo@gmail.com>
2025-03-26 21:07:04 +0100

    Fix styles with new ul and li page elements

--- src/components/Header/getHeader.ts
+++ src/components/Header/getHeader.ts
@@ -2,7 +2,7 @@ const getHeader = (): HTMLElement => {
   const header = document.createElement("header");
   header.classList.add("main-header");
 
-  header.innerHTML = "<h1>Game of Thrones</h1>";
+  header.innerHTML = "<h1 class='title'>Game of Thrones</h1>";
 
   return header;
 };
