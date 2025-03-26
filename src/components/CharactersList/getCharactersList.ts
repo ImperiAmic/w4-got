233c63e61c1b524144a33c6708ebc2141e629df4
Raimon <raimon.txollo@gmail.com>
2025-03-26 19:21:41 +0100

    Fix dependencies of getCharactersList function

--- src/components/CharactersList/getCharactersList.ts
+++ src/components/CharactersList/getCharactersList.ts
@@ -1,12 +1,6 @@
-import { characters } from "../../character/data/data.js";
-import getCharacterCard from "../CharacterCard/getCharacterCard.js";
-
 const getCharactersList = (): HTMLElement => {
   const charactersElement = document.createElement("ul");
   charactersElement.className = "characters";
-
-  const CharacterCard = getCharacterCard(characters[0]);
-  charactersElement.appendChild(CharacterCard);
 
   return charactersElement;
 };
