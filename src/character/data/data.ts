ca18a618837de6df87d73248c1716f99390323d3
Alex Uribe <repzyyog@gmail.com>
2025-03-27 19:30:09 +0100

    Add characters with factory functions

--- src/character/data/data.ts
+++ src/character/data/data.ts
@@ -1,51 +1,71 @@
-import { Adviser } from "../../adviser/types.js";
-import { Fighter } from "../../fighter/types.js";
-import { King } from "../../king/types.js";
-import { Squire } from "../../squire/types.js";
+import getAdviser from "../../adviser/factory/getAdviser.js";
+import getFighter from "../../fighter/factory/getFighter.js";
+import getKing from "../../king/factory/getKing.js";
+import getSquire from "../../squire/factory/getSquire.js";
+import { CharacterType } from "../types.js";
 
-export const characters: (King | Adviser | Fighter | Squire)[] = [
+const joffrey = getKing(
   {
     name: "Joffrey",
     lastName: "Baratheon",
     age: 19,
     portraitUrl: "/images/joffrey-baratheon.webp",
     portraitDescription: "Portrait of Joffrey Baratheon",
-    isAlive: true,
-    die: () => {},
-    yearsOfReign: 2,
-    phrase: "You are all going to die",
-    speak: () => {
-      return "";
-    },
   },
+  2,
+);
+
+const jaime = getFighter(
   {
     name: "Jaime",
     lastName: "Lannister",
     age: 44,
     portraitUrl: "/images/jaime-lannister.webp",
     portraitDescription: "Portrait of Jaime Lannister",
-    isAlive: true,
-    die: () => {},
-    weapon: "Oathkeeper",
-    dexterity: 7,
-    phrase: "First I punch, then I ask",
-    speak: () => {
-      return "";
-    },
   },
+  "Oathkeeper",
+  7,
+);
+
+const daenerys = getFighter(
   {
     name: "Daenerys",
     lastName: "Targaryen",
     age: 24,
     portraitUrl: "/images/daenerys-targaryen.webp",
     portraitDescription: "Portrait of Daenerys Targaryen",
-    isAlive: true,
-    die: () => {},
-    weapon: "Drogon, Rhaegal, and Viserion",
-    dexterity: 5,
-    phrase: "First I punch, then I ask",
-    speak: () => {
-      return "";
-    },
   },
+  "Drogon, Rhaegal, and Viserion",
+  5,
+);
+
+const tyrion = getAdviser(
+  {
+    name: "Tyrion",
+    lastName: "Lannister",
+    age: 40,
+    portraitUrl: "/images/tyrion-lannister.webp",
+    portraitDescription: "Portrait of Tyrion Lannister",
+  },
+  daenerys,
+);
+
+const bronn = getSquire(
+  {
+    name: "Bronn",
+    lastName: "Blackwater",
+    age: 35,
+    portraitUrl: "/images/bronn.webp",
+    portraitDescription: "Portrait of Bronn Blackwater",
+  },
+  jaime,
+  10,
+);
+
+export const characters: CharacterType[] = [
+  joffrey,
+  jaime,
+  daenerys,
+  tyrion,
+  bronn,
 ];
