e763baea906060fc85b99971ea3fb739f232a0c8
Alex Uribe <repzyyog@gmail.com>
2025-03-27 18:48:40 +0100

    Fix die property function

--- src/character/factory/getCharacter.ts
+++ src/character/factory/getCharacter.ts
@@ -1,11 +1,12 @@
-import killCharacter from "../killCharacter.js";
 import { Character, CharacterData } from "../types.js";
 
 const getCharacter = (characterData: CharacterData): Character => {
   const character: Character = {
     ...characterData,
     isAlive: true,
-    die: () => killCharacter(character),
+    die() {
+      this.isAlive = false;
+    },
     speak: () => "",
   };
 
