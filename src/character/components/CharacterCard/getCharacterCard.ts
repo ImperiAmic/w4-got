30eb3879e5af740480153d323972f05b57152ec1
Alex Uribe <repzyyog@gmail.com>
2025-03-28 22:26:55 +0100

    Add state info with thumbs up in character card

--- src/character/components/CharacterCard/getCharacterCard.ts
+++ src/character/components/CharacterCard/getCharacterCard.ts
@@ -4,9 +4,13 @@ const getCharacterCard = (character: Character): HTMLElement => {
   const characterCardElement = document.createElement("article");
 
   characterCardElement.innerHTML = `
-    <img src="${character.portraitUrl}" alt="${character.portraitDescription}" width="300" height="300">
+    <img class="character__portrait "src="${character.portraitUrl}" alt="${character.portraitDescription}" width="300" height="300">
     <div class="character__info">
       <h2 class="character__title">${character.name} ${character.lastName}</h2>
+      <div class="character__state">
+        <span>State:</span>
+        <img class="character__icon" src="/images/thumb-up-fill.svg" alt="thumb up icon" width="20" height="20">
+      </div>
     </div>
     `;
 
