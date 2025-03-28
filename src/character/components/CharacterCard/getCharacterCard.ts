8bec3f0cd2ca0260f54df4d76588f8862dbb89b5
Raimon <raimon.txollo@gmail.com>
2025-03-28 21:30:32 +0100

    Remove unused class

--- src/character/components/CharacterCard/getCharacterCard.ts
+++ src/character/components/CharacterCard/getCharacterCard.ts
@@ -4,7 +4,7 @@ const getCharacterCard = (character: Character): HTMLElement => {
   const characterCardElement = document.createElement("article");
 
   characterCardElement.innerHTML = `
-    <img class="character__portrait" src="${character.portraitUrl}" alt="${character.portraitDescription}" width="300" height="300">
+    <img src="${character.portraitUrl}" alt="${character.portraitDescription}" width="300" height="300">
     <div class="character__info">
       <h2 class="character__title">${character.name} ${character.lastName}</h2>
     </div>
