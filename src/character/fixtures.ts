fa6cc9de9e27177f15fee16357e630a65645b7b2
Alex Uribe <repzyyog@gmail.com>
2025-03-28 19:58:59 +0100

    Fix fighter tests with fixtures

--- src/character/fixtures.ts
+++ src/character/fixtures.ts
@@ -8,3 +8,12 @@ export const dancer: CharacterData = {
     "https://static.wikia.nocookie.net/red-rising/images/0/0c/Dancer-gorydamnreaper.jpg/revision/latest?cb=20190923184825",
   portraitDescription: "Dancer O'Faran Portrait",
 };
+
+export const kaladin: CharacterData = {
+  name: "Kaladin",
+  lastName: "Stormblessed",
+  age: 22,
+  portraitUrl:
+    "https://preview.redd.it/c03eods5x4aa1.jpg?auto=webp&s=fe3c7ca023b69ab42544100ed3504bdc925230eb",
+  portraitDescription: "Kaladin flying with Sylfrena as a spear",
+};
