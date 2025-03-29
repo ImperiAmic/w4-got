24faf70bbfdcb2b7cb043150a4496ed1c9fa1ff1
Alex Uribe <repzyyog@gmail.com>
2025-03-29 12:01:32 +0100

    Add tests to getCharacterCard new functionalities

--- src/character/components/CharacterCard/getCharacterCard.test.ts
+++ src/character/components/CharacterCard/getCharacterCard.test.ts
@@ -31,10 +31,62 @@ describe("Given the CharacterCard component", () => {
 
       screen.appendChild(CharacterCard);
 
-      const CardImage = screen.querySelector("img");
+      const CardImage = screen.querySelector(
+        ".character__portrait",
+      ) as HTMLImageElement;
 
       expect(CardImage).not.toBeNull();
       expect(CardImage?.alt).toBe(expectedImageDescription);
+    });
+
+    test("Then it should show a state with a thumb up icon when Felipe Sesto is alive inside a character card", () => {
+      const screen = document.createElement("div");
+      const expectedIconDescription = "thumb up icon";
+
+      const CharacterCard = getCharacterCard(king);
+
+      screen.appendChild(CharacterCard);
+
+      const CardIcon = screen.querySelector(
+        ".character__icon",
+      ) as HTMLImageElement;
+
+      expect(CardIcon).not.toBeNull();
+      expect(CardIcon?.alt).toBe(expectedIconDescription);
+    });
+
+    test("Then it should show a state with a thumb down icon when we kill Felipe Sesto inside a character card", () => {
+      const screen = document.createElement("div");
+      const expectedIconDescription = "thumb down icon";
+
+      const deadKing = { ...king };
+      deadKing.isAlive = false;
+
+      const CharacterCard = getCharacterCard(deadKing);
+
+      screen.appendChild(CharacterCard);
+
+      const CardIcon = screen.querySelector(
+        ".character__icon",
+      ) as HTMLImageElement;
+
+      expect(CardIcon).not.toBeNull();
+      expect(CardIcon?.alt).toBe(expectedIconDescription);
+    });
+
+    test("Then it should show an upside down portrait of Felipe Sesto when we kill him inside a character card", () => {
+      const screen = document.createElement("div");
+
+      const deadKing = { ...king };
+      deadKing.isAlive = false;
+
+      const CharacterCard = getCharacterCard(deadKing);
+
+      screen.appendChild(CharacterCard);
+
+      const CardImage = screen.querySelector(".character__portrait--reverse");
+
+      expect(CardImage).not.toBeNull();
     });
   });
 });
