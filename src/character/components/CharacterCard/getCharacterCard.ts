22e46bb7e2017ed9b53163479cef18e049dd41e6
Alex Uribe <repzyyog@gmail.com>
2025-03-29 13:29:37 +0100

    Add different emote for each type in character card

--- src/character/components/CharacterCard/getCharacterCard.ts
+++ src/character/components/CharacterCard/getCharacterCard.ts
@@ -6,6 +6,8 @@ const getCharacterCard = (character: Character): HTMLElement => {
   let iconUrl = "/images/thumb-up-fill.svg";
   let iconDescription = "thumb up icon";
 
+  let typeEmote: string;
+
   const portraitElement = document.createElement("img");
   portraitElement.classList.add("character__portrait");
   portraitElement.src = character.portraitUrl;
@@ -19,14 +21,39 @@ const getCharacterCard = (character: Character): HTMLElement => {
     portraitElement.classList.add("character__portrait--reverse");
   }
 
+  switch (character.speak()) {
+    case "You are all going to die":
+      typeEmote = "👑";
+      break;
+
+    case "First I punch, then I ask":
+      typeEmote = "🗡";
+      break;
+
+    case "I don't know why, but I think I'm going to die soon":
+      typeEmote = "🎓";
+      break;
+
+    case "I am a looser":
+      typeEmote = "🛡️";
+      break;
+
+    default:
+      typeEmote = "";
+      break;
+  }
+
   characterCardElement.innerHTML = `
     <div class="character__info">
       <h2 class="character__title">${character.name} ${character.lastName}</h2>
       <div class="character__data">
-        <span>Age: ${character.age} years</span>
-        <div class="character__state">
-          <span>State:</span>
-          <img class="character__icon" src="${iconUrl}" alt="${iconDescription}" width="20" height="20">
+        <span class="character__text">Age: ${character.age} years</span>
+        <div class="character__emotes">
+          <div class="character__state">
+            <span class="character__text">State:</span>
+            <img class="character__icon" src="${iconUrl}" alt="${iconDescription}" width="20" height="20">
+          </div>
+          <span class="character__type">${typeEmote}</span>
         </div>
       </div>
     </div>
